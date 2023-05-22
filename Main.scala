39e83ea24edf31b04c71ef18c04af461d336eced
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-05-22 21:05:17 +0200

    Routed

--- Main.scala
+++ Main.scala
@@ -1,26 +1,46 @@
 //> using scala "3.2"
 //> using dep "org.http4s::http4s-core:0.23.19"
+//> using dep "org.http4s::http4s-ember-server:0.23.19"
 
 package hlinx
 
 import cats.data.{Kleisli, OptionT}
 import cats.effect.{ExitCode, IO, IOApp}
+import com.comcast.ip4s.port
 import org.http4s.*
 import org.http4s.implicits.*
+import org.http4s.ember.server.EmberServerBuilder
 
 import scala.concurrent.duration.*
 
-object Main {
-  case class Foo(bar: String, baz: Int)
+object Main extends IOApp {
+  case class Greeter(who: String)
 
-  def main(args: Array[String]): Unit = {
-    val path = Root / "foo" / param[String]("bar") / param[Int]("baz")
-    val zz = hlinx("foo", param[String]("bar"), param[Int]("baz"))
-    //p"foo/${bar}/${baz}"
-    //println(zz.template)
-    println(path.asList)
-    val matchingpath = uri"/foo/bar/134"
-    println(path.to[Foo](matchingpath.path))
 
+  override def run(args: List[String]): IO[ExitCode] = {
+    EmberServerBuilder.default[IO]
+      .withPort(port"8080")
+      .withHttpApp(
+        Routed.builder[IO, Unit](Root / "hello" / param[String]("who"))
+          .get(
+            ContextRoutes {
+              case ContextRequest(((), ctx), req) =>
+                val greeter = ctx.to[Greeter]
+                OptionT.some[IO](Response[IO]().withEntity(s"Hello ${greeter.pathType.who}"))
+            }
+          ).buildHttpRoutes.orNotFound
+      )
+      .build.useForever
   }
+
+
+  /*val path = Root / "foo" / param[String]("bar") / param[Int]("baz")
+  val zz = hlinx("foo", param[String]("bar"), param[Int]("baz"))
+  //p"foo/${bar}/${baz}"
+  //println(zz.template)
+  println(path.asList)
+  val matchingpath = uri"/foo/bar/134"
+  println(path.to[Foo](matchingpath.path))*/
+
+
 }
---
+++ Routed.scala
@@ -0,0 +1,77 @@
+package hlinx
+
+import cats.Monad
+import cats.syntax.all.*
+import cats.data.{Kleisli, OptionT}
+import cats.effect.Sync
+import org.http4s.Uri.Path
+import org.http4s.headers.Allow
+import org.http4s.*
+
+import scala.deriving.Mirror
+
+type RoutedRequest[F[_], A, T] = ContextRequest[F, (A, RouteContext[T])]
+type Routed[F[_], A, T] = ContextRoutes[(A, RouteContext[T]), F]
+
+object Routed {
+  def of[F[_] : Monad, A, T <: Tuple](template: HLinx[T], methods: Map[Method, Routed[F, A, T]]): ContextRoutes[A, F] =
+    ContextRoutes[A, F] {
+      case ContextRequest(a, req) =>
+        val path = req.pathInfo
+        template.extract(path) match
+          case Left(value) => OptionT.none //todo: this needs work
+          case Right(value) => {
+            val route: Routed[F, A, T] = methods.getOrElse(
+              req.method,
+              ContextRoutes(
+                _ => OptionT.some[F](Response[F](Status.MethodNotAllowed).putHeaders(Allow(methods.keySet)))
+              )
+            )
+            val templateContext = RouteContext(value)
+            route(ContextRequest(a -> templateContext, req))
+          }
+    }
+
+  def route[F[_], A, T <: Tuple](template: HLinx[T]) = BuilderStep1[F, A, T](template)
+
+  def builder[F[_], A] = PartiallyApplied[F, A]()
+
+  sealed trait WithBuilderStep2[F[_], A, T <: Tuple] {
+    def withMethod(method: Method, routed: Routed[F, A, T]): BuilderStep2[F, A, T]
+
+    def get(routed: Routed[F, A, T]) = withMethod(Method.GET, routed)
+
+    def post(routed: Routed[F, A, T]) = withMethod(Method.POST, routed)
+
+    def put(routed: Routed[F, A, T]) = withMethod(Method.PUT, routed)
+
+    def delete(routed: Routed[F, A, T]) = withMethod(Method.DELETE, routed)
+  }
+
+  case class PartiallyApplied[F[_], A]() {
+    def apply[T <: Tuple](linx: HLinx[T]): BuilderStep1[F, A, T] = BuilderStep1(linx)
+  }
+
+  case class BuilderStep1[F[_], A, T <: Tuple](template: HLinx[T]) extends WithBuilderStep2[F, A, T] {
+    def withMethod(method: Method, routed: Routed[F, A, T]) = BuilderStep2(template, Map(method -> routed))
+  }
+
+  case class BuilderStep2[F[_], A, T <: Tuple](template: HLinx[T], methods: Map[Method, Routed[F, A, T]]) extends WithBuilderStep2[F, A, T] {
+    def withMethod(method: Method, routed: Routed[F, A, T]) = BuilderStep2(template, methods.updated(method, routed))
+
+    def build(using M: Monad[F]): ContextRoutes[A, F] = Routed.of(template, methods)
+
+    def buildHttpRoutes(using ev: Unit =:= A, M: Monad[F]): HttpRoutes[F] = {
+      val route = Routed.of(template, methods)
+      Kleisli { req =>
+        route(ContextRequest(ev(()), req))
+      }
+    }
+  }
+}
+
+case class RouteContext[T](pathType: T) {
+  inline def to[A](using m: Mirror.ProductOf[A]): RouteContext[A] =
+    pathType match
+      case t: m.MirroredElemTypes => RouteContext(m.fromTuple(t))
+}

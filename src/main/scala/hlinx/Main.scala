5f65231f95a81109f03708dd0bb8f179cee2e488
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-05-25 14:59:08 +0200

    Move some code around

--- src/main/scala/hlinx/Main.scala
+++ src/main/scala/hlinx/Main.scala
@@ -1,9 +1,3 @@
-//> using scala "3.2"
-//> using dep "org.http4s::http4s-core:0.23.19"
-//> using dep "org.http4s::http4s-ember-server:0.23.19"
-//> using dep "org.http4s::http4s-dsl:0.23.19"
-//> using dep "org.slf4j:slf4j-simple:2.0.7"
-
 package hlinx
 
 import cats.arrow.FunctionK

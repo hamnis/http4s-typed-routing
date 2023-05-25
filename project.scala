d11a543f42448e2ea6acdc23210c7729af8247c5
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-05-25 15:13:51 +0200

    register test framework

--- project.scala
+++ project.scala
@@ -3,3 +3,4 @@
 //> using dep "org.http4s::http4s-ember-server:0.23.19"
 //> using dep "org.http4s::http4s-dsl:0.23.19"
 //> using dep "org.slf4j:slf4j-simple:2.0.7"
+//> using test.dep org.scalameta::munit::0.7.29
